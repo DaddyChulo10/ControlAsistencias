47df9689587030f7e0e4c3ff3abcccfec67bf287
edgar <edgtarkalim@gmail.com>
2024-04-28 17:54:15 -0600

    Script para activar la camara y leer el código QR

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,24 +2,59 @@
 @section('content')
     <script src="https://cdn.jsdelivr.net/npm/jsqr/dist/jsQR.min.js"></script>
 
-    <video id="video" width="640" height="480" autoplay></video>
     <button type="button" class="btn btn-info" id="startButton">Activar Cámara</button>
+    <br>
 
+    <video id="video" width="300" height="200" autoplay></video>
+    <canvas id="canvas" style="display: none;"></canvas>
     <script>
         const video = document.getElementById('video');
+        const canvas = document.getElementById('canvas');
+        const context = canvas.getContext('2d');
+
+        let scanning = false;
 
         document.getElementById('startButton').addEventListener('click', () => {
-            // Solicitar acceso a la cámara
+            if (!scanning) {
+                startScanning();
+            } else {
+                stopScanning();
+            }
+        });
+
+        function startScanning() {
             navigator.mediaDevices.getUserMedia({
-                    video: true
+                    video: {
+                        facingMode: 'environment'
+                    }
                 })
                 .then(stream => {
-                    // Mostrar el flujo de la cámara en el elemento de video
                     video.srcObject = stream;
+                    video.play();
+                    scanning = true;
+                    scanFrame();
                 })
-                .catch(error => {
-                    console.error('Error al acceder a la cámara:', error);
-                });
-        });
+                .catch(err => console.error('Error al acceder a la cámara: ', err));
+        }
+
+        function stopScanning() {
+            video.pause();
+            video.srcObject.getTracks()[0].stop();
+            scanning = false;
+        }
+
+        function scanFrame() {
+            if (!scanning) return;
+
+            context.drawImage(video, 0, 0, canvas.width, canvas.height);
+            const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+            const code = jsQR(imageData.data, imageData.width, imageData.height);
+
+            if (code) {
+                alert('Mensaje del código QR: ' + code.data);
+            }
+
+            requestAnimationFrame(scanFrame);
+        }
     </script>
 @endsection
